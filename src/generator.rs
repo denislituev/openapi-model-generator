8d627900161e6835e0b355795f184fad1cae950b
dzianis.lituyeu <dzianis.lituyeu@constructor.tech>
2025-05-14 11:51:30 +0300

    Fix: reserved words usage

--- src/generator.rs
+++ src/generator.rs
@@ -1,5 +1,19 @@
 use crate::{models::Model, Result};
 
+const RUST_RESERVED_KEYWORDS: &[&str] = &[
+    "as", "break", "const", "continue", "crate", "else", "enum", "extern", "false", "fn",
+    "for", "if", "impl", "in", "let", "loop", "match", "mod", "move", "mut", "pub", "ref",
+    "return", "self", "Self", "static", "struct", "super", "trait", "true", "type", "unsafe",
+    "use", "where", "while",
+
+    "abstract", "become", "box", "do", "final", "macro", "override", "priv", "try",
+    "typeof", "unsized", "virtual", "yield",
+];
+
+fn is_reserved_word(string_to_check: &str) -> bool {
+    RUST_RESERVED_KEYWORDS.contains(&string_to_check)
+}
+
 pub fn generate_rust_code(models: &[Model]) -> Result<String> {
     let mut code = String::new();
 
@@ -24,6 +38,11 @@ pub fn generate_rust_code(models: &[Model]) -> Result<String> {
                 _ => &field.field_type,
             };
 
+            let mut lowercased_name = field.name.to_lowercase();
+            if is_reserved_word(&lowercased_name) {
+                lowercased_name = format!("r#{}", lowercased_name)
+            }
+
             code.push_str(&format!(
                 "    #[serde(rename = \"{}\")]\n",
                 field.name.to_lowercase()
@@ -32,13 +51,13 @@ pub fn generate_rust_code(models: &[Model]) -> Result<String> {
             if field.is_required {
                 code.push_str(&format!(
                     "    pub {}: {},\n",
-                    field.name.to_lowercase(),
+                    lowercased_name,
                     field_type
                 ));
             } else {
                 code.push_str(&format!(
                     "    pub {}: Option<{}>,\n",
-                    field.name.to_lowercase(),
+                    lowercased_name,
                     field_type
                 ));
             }
